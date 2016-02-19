74f6da41c539fb51e240f22c6be0b5b1d4ae0ca2
favalos2 <avalosf@live.com>
2016-02-19 11:57:07 -0700

    first check in

---
+++ CSE360Assignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,61 @@
+package cse360assign3;
+
+/** description of this class is needed
+ * @author Fernando Avalos       
+ * @class CSE 360
+ * @pin 112
+ * @version feb 19, 2016
+ */
+
+
+/** This class mimics the arithmetic done by a calculator */
+public class Calculator {
+
+	private int total;
+	
+	/** This is the constructor */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** this return the total
+	 * @return the total amount
+	 * */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** We are adding values together
+	 * @param value 
+	 * */
+	public void add (int value) {
+		
+	}
+	
+	/** We are subtracting values
+	 * @param value 
+	 * */
+	public void subtract (int value) {
+		
+	}
+	
+	/**we are multiplying values together
+	 * @param value
+	 * */
+	public void multiply (int value) {
+		
+	}
+	
+	/** we are dividing values together
+	 * @param value
+	 * */
+	public void divide (int value) {
+		
+	}
+	
+	/** we are returning the history of what we have done
+	 * @return */
+	public String getHistory () {
+		return "";
+	}
+}
